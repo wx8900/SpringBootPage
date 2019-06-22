d6b0333921f66f2ff84adab6262e5d71b7a5881a
Jack Cai <cwx8900@gmail.com>
2019-06-22 05:29:44 -0700

    modify Redis API

--- src/main/java/com/demo/test/controllers/RedisController.java
+++ src/main/java/com/demo/test/controllers/RedisController.java
@@ -3,34 +3,31 @@ package com.demo.test.controllers;
 import com.demo.test.domain.Constant;
 import com.demo.test.domain.ResultInfo;
 import com.demo.test.domain.Student;
-import com.demo.test.utils.RedisUtils;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@Validated
 @RequestMapping("/v1/api/students")
-@Slf4j
 public class RedisController {
 
     Logger logger = Logger.getLogger(RedisController.class);
 
-    /*@Autowired
-    private RedisUtils redisUtils;*/
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    @PostMapping(value="/redisAdd")
-    public void saveRedis(){
-        stringRedisTemplate.opsForValue().set("admin2019062211","test062211");
+    @PostMapping(value = "/redisAdd")
+    public void saveRedis() {
+        stringRedisTemplate.opsForValue().set("admin2019062211", "test062211");
     }
 
-    @GetMapping(value="/redisGet")
-    public String getRedis(){
+    @GetMapping(value = "/redisGet")
+    public String getRedis() {
         return stringRedisTemplate.opsForValue().get("admin2019062211");
     }
 
@@ -48,8 +45,8 @@ public class RedisController {
      * 获取用户
      */
     @GetMapping("/redis/get")
-    public ResultInfo getUser() throws Exception {
-        Object uu = stringRedisTemplate.opsForValue().get("uUserTest0622");
+    public ResultInfo getUser(String key) throws Exception {
+        Object uu = stringRedisTemplate.opsForValue().get(key);
         logger.info("redis中获取数据：[{}]" + uu);
         logger.error("redis中获取数据：[{}]" + uu);
         logger.warn("redis中获取数据：[{}]" + uu);
