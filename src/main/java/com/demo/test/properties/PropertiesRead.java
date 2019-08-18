7cf0837f9f16671b5824b0c8c9d33d052bd5f5b9
Jack Cai <cwx8900@gmail.com>
2019-08-18 04:09:46 -0700

    update read properties file

---
+++ src/main/java/com/demo/test/properties/PropertiesRead.java
@@ -0,0 +1,36 @@
+package com.demo.test.properties;
+
+import java.io.*;
+import java.util.Properties;
+
+/**
+ * Util class for reading properties file
+ *
+ * @author Jack
+ * @date 2019/08/18 01:30 AM
+ */
+public class PropertiesRead {
+
+    public static void main(String[] args) {
+
+        try (InputStream input = PropertiesRead.class.getClassLoader().getResourceAsStream("application.properties")) {
+
+            Properties prop = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            //get the property value and print it out
+            System.out.println(prop.getProperty("spring.profiles.active"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/demo/test/properties/PropertiesWrite.java
@@ -0,0 +1,47 @@
+package com.demo.test.properties;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class PropertiesWrite {
+
+    public static void main(String[] args) {
+
+        /*final File file = new File("application.properties");
+        try (final Writer writer = new BufferedWriter(new FileWriter(file, true))) {
+            Properties prop = new Properties();
+            // set the properties value
+            prop.setProperty("db.url", "localhost");
+            prop.setProperty("db.user", "mkyong");
+            prop.setProperty("db.password", "password");
+            // save properties to project root folder
+            prop.store(writer, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        try {
+            Properties grades = new Properties();
+
+            grades.setProperty("Geometry", "2000");
+            grades.setProperty("Algebra", "20");
+            grades.setProperty("Physics", "18");
+            grades.setProperty("Chemistry", "17");
+            grades.setProperty("Biology", "19");
+
+            // Save the grades properties using store() and an output stream
+            String desktop = System.getProperty ("user.home") + "/Desktop/";
+            FileOutputStream out = new FileOutputStream(
+                    //desktop +
+                    "/src/main/sources/example.properties");
+            grades.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
