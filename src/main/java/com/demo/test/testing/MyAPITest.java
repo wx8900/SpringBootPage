a2ac3348b55473fe3d2f17bcece5d7f7f0bdd5af
Jack Cai <cwx8900@gmail.com>
2019-10-08 20:33:19 +0800

    Add a link for API testing

--- src/main/java/com/demo/test/testing/MyAPITest.java
+++ src/main/java/com/demo/test/testing/MyAPITest.java
@@ -16,7 +16,8 @@ public class MyAPITest {
 
     public static void main(String[] args) {
         //String url = "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?";
-        String url = "http://cache.video.iqiyi.com/jp/avlist/202861101/1/?callback=jsonp9";
+        //String url = "http://cache.video.iqiyi.com/jp/avlist/202861101/1/?callback=jsonp9";
+        String url = "https://www.apiopen.top/journalismApi";
         // 参数要放入map
         //String url = "http://www.tudou.com/tvp/getMultiTvcCodeByAreaCode.action?type=3&app=4&codes=Lqfme5hSolM&areaCode=320500&jsoncallback=__TVP_getMultiTvcCodeByAreaCode";
         Map<String, String> map = new HashMap<>();
