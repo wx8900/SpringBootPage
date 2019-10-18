6dbd0e2f021c93d12ca322d50235634a313aabf4
Jack Cai <cwx8900@gmail.com>
2019-10-18 09:13:52 +0800

    Add backup, restore and delete database class

---
+++ src/main/java/com/demo/test/databasebackup/DBBackupImpl.java
@@ -0,0 +1,90 @@
+package com.demo.test.databasebackup;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 备份操作、恢复、删除工具类
+ */
+public class DBBackupImpl extends DBBackupUtils {
+
+    /**
+     * 备份操作工具
+     */
+    public boolean dbBackUp() {
+        //最终文件名
+        String finalFileName = System.currentTimeMillis() + fileName;
+        String pathSql = backPath + finalFileName;
+        File fileSql = new File(pathSql);
+        //创建备份sql文件
+        if (!fileSql.exists()) {
+            try {
+                fileSql.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        sb.append(toolPath + "mysqldump");
+        sb.append(" -h" + host);
+        sb.append(" -u" + root);
+        sb.append(" -p" + pwd);
+        sb.append(" " + dbName + " >");
+        sb.append(pathSql);
+        System.out.println("cmd命令为：" + sb.toString());
+        System.out.println("开始备份：" + dbName);
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process process = runtime.exec("cmd /c " + sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 数据恢复操作工具
+     */
+    public boolean dbRestore(String fileName) throws IOException {
+        String filePath = backPath + fileName;
+        StringBuilder sb = new StringBuilder();
+        sb.append(toolPath + "mysql");
+        sb.append(" -h" + host);
+        sb.append(" -u" + root);
+        sb.append(" -p" + pwd);
+        sb.append(" " + dbName + " <");
+        sb.append(filePath);
+        System.out.println("cmd命令为：" + sb.toString());
+        System.out.println("开始还原数据");
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec("cmd /c" + sb.toString());
+        //插入数据库备份恢复记录表
+        try (InputStream is = process.getInputStream();
+             BufferedReader bf = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            String line = null;
+            while ((line = bf.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 删除数据备份
+     */
+    public String remove(String fileName) {
+        final String file = DBBackupUtils.backPath + fileName;
+        File del_file = new File(file);
+        if (del_file.isFile()) {
+            if (del_file.delete() == false) {
+                return "删除失败";
+            }
+        }
+        return "删除成功";
+    }
+
+}
---
+++ src/main/java/com/demo/test/databasebackup/DBBackupUtils.java
@@ -0,0 +1,36 @@
+package com.demo.test.databasebackup;
+
+/**
+ *
+ */
+public class DBBackupUtils {
+
+    /**
+     * 数据库安装地址
+     */
+    public static final String host = "127.0.0.1";
+    /**
+     * 操作数据库
+     */
+    public static final String dbName = "***";
+    /**
+     * 用户名
+     */
+    public static final String root = "***";
+    /**
+     * 密码
+     */
+    public static final String pwd = "***";
+    /**
+     * 备份位置
+     */
+    public static final String backPath = "D:/workPro/work/sql/";
+    /**
+     * 备份文件命名规则
+     */
+    public static final String fileName = "work.sql";
+    /**
+     * MySQL命令工具 绝对路径
+     */
+    public static final String toolPath = "D:/phpstudy/PHPTutorial/MySQL/bin/";
+}
