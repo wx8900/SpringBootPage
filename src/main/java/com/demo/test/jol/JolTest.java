fc963b89a9438918016689f087b8f137ad114c86
Jack Cai <cwx8900@gmail.com>
2020-08-14 11:29:48 +0800

    add comments

---
+++ src/main/java/com/demo/test/jol/JolTest.java
@@ -0,0 +1,10 @@
+package com.demo.test.jol;
+
+import org.openjdk.jol.info.ClassLayout;
+
+public class JolTest {
+    public static void main(String[] args) {
+        JolTest jolTest = new JolTest();
+        System.out.println(ClassLayout.parseInstance(jolTest).toPrintable());
+    }
+}
