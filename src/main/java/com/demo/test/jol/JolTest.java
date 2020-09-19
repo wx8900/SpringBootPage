278d6795f6099fb97831fd0a29194527dce5fb56
Jack Cai <cwx8900@gmail.com>
2020-09-19 22:40:33 +0800

    add jol

--- src/main/java/com/demo/test/jol/JolTest.java
+++ src/main/java/com/demo/test/jol/JolTest.java
@@ -10,5 +10,21 @@ public class JolTest {
         synchronized (jolTest) {
             System.out.println(ClassLayout.parseInstance(jolTest).toPrintable());
         }
+
+        int size = 10;
+        List<Integer> list = new ArrayList<Integer>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        //虚拟机信息
+        out.println(VMSupport.vmDetails());
+        //打印类内部的占用
+        out.println(ClassLayout.parseClass(ArrayList.class).toPrintable());
+        //打印实例内部的占用
+        out.println(ClassLayout.parseClass(ArrayList.class).toPrintable(list));
+        //打印实例外部的占用
+        out.println(GraphLayout.parseInstance(list).toPrintable());
+        //打印实例各个依赖的占用,并汇总
+        out.println(GraphLayout.parseInstance(list).toFootprint())
     }
 }
