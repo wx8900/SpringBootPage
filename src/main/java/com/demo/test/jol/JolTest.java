e67aff40e018d4e51f9d8e220d31629148c4c304
Jack Cai <cwx8900@gmail.com>
2020-10-03 23:28:00 +0800

    add comments

--- src/main/java/com/demo/test/jol/JolTest.java
+++ src/main/java/com/demo/test/jol/JolTest.java
@@ -1,6 +1,11 @@
 package com.demo.test.jol;
 
 import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
+import sun.misc.VMSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JolTest {
     public static void main(String[] args) {
@@ -12,19 +17,19 @@ public class JolTest {
         }
 
         int size = 10;
-        List<Integer> list = new ArrayList<Integer>(size);
+        List<Integer> list = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
             list.add(i);
         }
         //虚拟机信息
-        out.println(VMSupport.vmDetails());
+        System.out.println(VMSupport.getAgentProperties());
         //打印类内部的占用
-        out.println(ClassLayout.parseClass(ArrayList.class).toPrintable());
+        System.out.println(ClassLayout.parseClass(ArrayList.class).toPrintable());
         //打印实例内部的占用
-        out.println(ClassLayout.parseClass(ArrayList.class).toPrintable(list));
+        System.out.println(ClassLayout.parseClass(ArrayList.class).toPrintable(list));
         //打印实例外部的占用
-        out.println(GraphLayout.parseInstance(list).toPrintable());
+        System.out.println(GraphLayout.parseInstance(list).toPrintable());
         //打印实例各个依赖的占用,并汇总
-        out.println(GraphLayout.parseInstance(list).toFootprint())
+        System.out.println(GraphLayout.parseInstance(list).toFootprint());
     }
 }
